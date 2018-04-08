6b7141e28d40595d5b19869893c58695c99480e8
Ilshat <g.ilshat.98@mail.ru>
2018-04-09 02:00:09 +0300

    11

--- app/src/main/java/com/example/ilshat/innoattendance/MainActivity.java
+++ app/src/main/java/com/example/ilshat/innoattendance/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);hashCode();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
